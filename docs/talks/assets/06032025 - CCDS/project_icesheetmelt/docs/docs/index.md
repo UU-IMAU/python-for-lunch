f797266c6290d636229828db8d93ae0a1aa58cbf
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2025-03-06 16:03:32 +0100

    Add example folder

---
+++ docs/talks/assets/06032025 - CCDS/project_icesheetmelt/docs/docs/index.md
@@ -0,0 +1,9 @@
+# project_icesheetmelt documentation!
+
+## Description
+
+Study melt of ice shelves
+
+## Commands
+
+The Makefile contains the central entry points for common tasks related to this project.
---
+++ docs/talks/assets/06032025 - CCDS/project_icesheetmelt/src/dummy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def add_numbers(a, b):
+    "This function returns the sum of a and b"
+    sumab = a + b
+
+    return sumab
+
+
+def subtract_numbers(a, b):
+    "This function returns a minus b"
+
+    subab = a - b
+    return subab
